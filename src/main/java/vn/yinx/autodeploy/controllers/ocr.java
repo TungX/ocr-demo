5dca670d1b8d8628788324fd8317442e004e3ac5
YinX <blue.rose.hut@gmail.com>
2020-07-01 18:24:04 +0700

    deploy ocr

--- src/main/java/vn/yinx/autodeploy/controllers/ocr.java
+++ src/main/java/vn/yinx/autodeploy/controllers/ocr.java
@@ -1,13 +1,10 @@
 package vn.yinx.autodeploy.controllers;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.Random;
 
 import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
@@ -16,36 +13,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import vn.yinx.autodeploy.models.IdCard;
 import vn.yinx.autodeploy.util.HttpUtils;
 
 @Controller
 @RequestMapping("/api/v1/ocr")
 public class ocr {
+	private Random rd = new Random();
+
 	@PostMapping("")
 	public ResponseEntity<JSONObject> getAll(@RequestBody IdCard idCard) {
 		// Save
 		JSONObject res = new JSONObject();// value return
 		res.put("status", 1);// default: success
-		byte[] data = Base64.getDecoder()
-                .decode(idCard.getImage().getBytes(StandardCharsets.UTF_8));
-	    FileOutputStream fos;
+
 		try {
-			fos = new FileOutputStream(new File("C:\\\\Users\\\\admin\\\\AndroidStudioProjects\\\\FPT_ID_CARD_DETECT\\\\app\\\\src\\\\main\\\\res\\\\drawable\\\\myImage.jpg"));
-			fos.write(data); 
-		    fos.close();
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} 
-	    
-	    File fo = new File(
-				"C:\\Users\\admin\\AndroidStudioProjects\\FPT_ID_CARD_DETECT\\app\\src\\main\\res\\drawable\\myImage.jpg");
-		try {
-//			Files.write(fo.toPath(), data);
-			JSONObject rs = HttpUtils.sendFile("https://api.fpt.ai/vision/idr/vnm", "vCtTr8rKRVKgHlQuoijreDNF3Mc0inhT",
+			byte[] data = Base64.getDecoder().decode(idCard.getImage().getBytes(StandardCharsets.UTF_8));
+			File fo = new File("images/" + System.currentTimeMillis() + "." + rd.nextLong() + ".jpg");
+			if (!fo.getParentFile().isDirectory()) {
+				fo.getParentFile().mkdirs();
+			}
+			FileOutputStream fos = new FileOutputStream(fo);
+			fos.write(data);
+			fos.close();
+			long start = System.currentTimeMillis();
+			JSONObject rs = HttpUtils.sendFile("https://api.fpt.ai/vision/idr/vnm", "hmHH4mRRoYJZkIimpulm8pQ8X0uV1N2s",
 					fo);
+			System.out.println("Time request: " + (System.currentTimeMillis() - start));
+			System.out.println(rs);
+			if(rs.get("message").toString().toLowerCase().equals("api rate limit exceeded.")) {
+				//Change key
+			}
 			res.put("data", rs.get("data"));// put data by key "data"
 			return ResponseEntity.ok(res);
 		} catch (Exception e) {
